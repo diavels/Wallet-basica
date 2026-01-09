8cc9b8079fe8842fac6be95180bc9d3815e28795
diavels <fabian.vigorena873@gmail.com>
2026-01-09 11:00:40 -0300

    Add files via upload

---
+++ wallet/js/main.js
@@ -0,0 +1,35 @@
+//logica del login
+
+/* validar credenciales - mostrar mensaje alert
+si son incorrectas */
+
+//credenciales
+const username = "fabian";
+const password = "admin";
+
+le
+
+//logica de menu principal
+
+/*agregar evento a los botones principales, 
+al hacer click, debe aparecer una leyenda diciendo:
+"redirigiendo a x" */
+
+//logica de deposito
+
+/* agregar evento al btn realizar deposito
+- actualizar el saldo con el monto depositado */
+
+//logica de enviar dinero
+
+/*evento al boton de agregar nuevo contacto
+- abrir formulario emergente para agregar contacto
+- seleccionar contacto
+- hacer click en enviar dinero y mostrar mensaje de
+ confirmacion, monto, y que actualice el saldo */
+
+// logica de ultimos movimientos
+
+/* 
+- mostrar lista de movimientos realizados, con los detalles
+ */
