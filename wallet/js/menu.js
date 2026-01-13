5d86177ac74dc5340ccbf8370a30ec4c5a676c9f
diavels <fabian.vigorena873@gmail.com>
2026-01-13 18:56:57 -0300

    Refactor menu button redirection logic

--- wallet/js/menu.js
+++ wallet/js/menu.js
@@ -64,3 +64,22 @@ event.preventDefault();
     }, 1000);
 });
 
+// PARA MOSTRAR EL SALDO
+// llama a la lista, luego recata el valor del saldo asociandolo a un id
+
+function mostrarSaldo() {
+    //llama la lista
+    const datos = JSON.parse(localStorage.getItem('alekWalletData'));
+    //asocia el id del saldo para mostrarlo dsp
+    const displaySaldo = document.getElementById('saldo'); 
+    
+    //condicional en caso de que no tenga saldo
+    if (datos && displaySaldo) {
+        displaySaldo.textContent = `$${datos.saldo.toLocaleString()}`;
+    } else if (displaySaldo) {
+        displaySaldo.textContent = "$0";
+    }
+}
+//sive para que el codigo se active dsp de que el dom este listo.
+document.addEventListener('DOMContentLoaded', mostrarSaldo);
+
